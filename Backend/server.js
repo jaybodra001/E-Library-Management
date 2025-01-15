19c993f0b26829280fe7c8f0887964b30fe7201a
jay bodra <jaybodra01@gmail.com>
2025-01-15 12:08:37 +0530

    production ready app

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import { ENV_VARS } from "./config/envVars.js";
 import authRoutes from "./routes/auth.route.js"
@@ -7,11 +8,17 @@ import authRoutes from "./routes/auth.route.js"
 const app = express()
 
 const PORT = ENV_VARS.PORT
-
+const __dirname = path.resolve()
 app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/v1/auth",authRoutes)
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
 
 app.listen(PORT,() => {
     console.log("Server is running on http://localhost:"+PORT)
